3093c9eefa906a604f9306997acbdfde0cfb173b
maksimiliani <macseg@gmail.com>
2021-05-05 22:23:29 +0300

    Update introversion-tweak.js

--- introversion/introversion-tweak.js
+++ introversion/introversion-tweak.js
@@ -8,13 +8,13 @@
         $("#rec312119903").append($('<div class="addgif06"></div>'));
 
         function tapHandler01( event ){
-            $(".addgif01").css({top: Math.random()*$("#rec312119903").height()*0.33, left: Math.random()*$(window).width()*0.66});
+            $(".addgif01").css({top: 100% + Math.random()*$("#rec312119903").height()*10%, left: Math.random()*$(window).width()*10% - 10%});
         }
         function tapHandler02( event ){
-            $(".addgif02").css({top: Math.random()*$("#rec312119903").height()*0.33 + $("#rec192970385").height()/2, left: Math.random()*$(window).width()*0.66});
+            $(".addgif02").css({top: 40% - Math.random()*$("#rec312119903").height()*30%, left: 30% + Math.random()*$(window).width()*15%});
         }
         function tapHandler03( event ){
-            $(".addgif03").css({top: Math.random()*$("#rec312119903").height()*0.33, left: Math.random()*$(window).width()*0.66});
+            $(".addgif03").css({top: Math.random()*$("#rec312119903").height()*0.33, right: Math.random()*$(window).width()*0.66});
         }
         function tapHandler04( event ){
             $(".addgif04").css({top: Math.random()*$("#rec312119903").height()*0.33 + $("#rec192970385").height()/2, left: Math.random()*$(window).width()*0.66});
@@ -40,7 +40,7 @@
         $('.addgif05').on( "vclick", "p", tapHandler05);
         $('.addgif06').on( "vclick", "p", tapHandler06);
 
-        setTimeout(()=> { update_figs() }, 5000);
+        setTimeout(()=> { update_figs() }, 2000);
         function update_figs() {
             console.log ("opacity");
             $(".addgif01").css({opacity: 1});
